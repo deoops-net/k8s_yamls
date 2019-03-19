cdbe4bfaa5070ec076c75f9e5cb14e1138a69695
yuyang <yuyang@xiaoyun.com>
2019-03-19 10:15:58 +0800

    add deploy

--- misc.yaml/get_congimap.go
+++ misc.yaml/get_congimap.go
@@ -9,17 +9,28 @@ import (
 
 // It's a short code for getting config map from /var/lib/myconfig
 func main() {
-	fd, err := os.Open("/var/lib/myconfig")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fd.Close()
+	fd, err := os.Open("/var/lib/myconfig/mongoHosts")
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("error: ", r)
+		}
+		log.Println("close file")
+		fd.Close()
+	}()
 
-	c, err := ioutil.ReadAll(fd)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+	} else {
+		c, err := ioutil.ReadAll(fd)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(string(c))
 	}
 
-	log.Println(string(c))
+	http.HandleFunc("/health", func(res http.ResponseWriter, req *http.Request) {
+		res.WriteHeader(200)
+	})
+
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
